f8ef301c18f9c1033313009fd91d8175b9760a3c
aBock4 <arbockhorst@gmail.com>
2023-02-06 16:29:23 -0500

    Initial Commit

---
+++ FleetTracker/src/main/java/com/fleettracker/FleetTracker/FleetTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.fleettracker.FleetTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetTrackerApplication.class, args);
+	}
+
+}
---
+++ FleetTracker/src/test/java/com/fleettracker/FleetTracker/FleetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleettracker.FleetTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
