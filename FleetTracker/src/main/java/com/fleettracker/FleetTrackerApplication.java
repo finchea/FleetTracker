01d77b1b3278a9386a0e042bc116b33d72e7b8b9
aBock4 <arbockhorst@gmail.com>
2023-02-07 13:07:53 -0500

    Changed Package name

--- FleetTracker/src/main/java/com/fleettracker/FleetTrackerApplication.java
+++ FleetTracker/src/main/java/com/fleettracker/FleetTrackerApplication.java
@@ -1,4 +1,4 @@
-package com.fleettracker.FleetTracker;
+package com.fleettracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
